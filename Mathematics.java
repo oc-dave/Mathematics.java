2766366e7301189153c10204700079347e9293b3
oc-dave <135023771+oc-dave@users.noreply.github.com>
2023-06-08 12:35:27 +0100

    Add files via upload

---
+++ Mathematics.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+	
+	public class Mathematics {
+	
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.print("Enter the first integer: ");
+	int number1 = scanner.nextInt();
+	
+	System.out.print("Enter the second integer: ");
+	int number2 = scanner.nextInt();
+	
+	int square1 = (number1 * number1);
+	
+	int square2 = (number2 * number2);
+	
+	int sumOfSquares = (square1 + square2);
+	
+	int differenceOfSquares = (square1 - square2);
+	
+	
+	System.out.println("Square of the first number: ";
+	
+        System.out.println("Square of the second number: ");
+        
+        System.out.println("Sum of their squares: ");
+        
+        System.out.println("Difference of their squares: ");
+	
+	
+
+
+
+	}
+	
+}	
